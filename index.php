0cf4ce7a878037b7b51cab71e104e3b3e6825d14
unknown <mstrp2ez@gmail.com>
2020-10-02 22:42:23 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<header>
+	<h1>Ludum Dare 47</h1>
+	<h2>Game title</h2>
+	<small>Made by Christoffer Södergren 2020</small>
+</header>
+<main>
+	<section>
+		<h2>Chapter 1</h2>
+		<p>Pickled blue bottle adaptogen, hammock YOLO you probably haven't heard of them cronut sustainable. Authentic adaptogen shabby chic vape pug ennui portland. Cred disrupt lyft poutine, fashion axe taxidermy deep v tofu organic kale chips normcore heirloom schlitz. Irony sartorial kitsch austin 8-bit, humblebrag celiac. Ennui cronut blue bottle small batch, distillery man braid YOLO enamel pin church-key hoodie pinterest DIY normcore paleo. Vexillologist taxidermy put a bird on it pickled, you probably haven't heard of them distillery four dollar toast humblebrag copper mug tumeric neutra salvia. Hot chicken leggings scenester schlitz, ramps godard pitchfork green juice man braid air plant pinterest neutra fixie williamsburg.</p>
+
+		<p><strong>Instructions:</strong> Press up, down, left, right, a, b, start, select </p>
+	</section>
+	<section>
+		<h2>Chapter 2</h2>
+		<canvas></canvas>
+	</section>
+	
+</main>
+<footer>
+
+</footer>
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
